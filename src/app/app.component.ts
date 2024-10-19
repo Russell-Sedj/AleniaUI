12e43e6eb20b0bec0e4b19844af98a2e75cde454
NeroBoyka <russelldossouyovo@gmail.com>
2024-10-20 00:27:37 +0200

    code pour differencier les header (connexion et autre)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RouterLink } from '@angular/router';
-import { RouterLinkActive } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
-  // styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css', '../../public/assets/Css/styles.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'essai';
+  currentUrl: string = ''; // Initialisation de currentUrl avec une chaîne vide
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Écouter les événements de navigation pour mettre à jour currentUrl
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentUrl = event.urlAfterRedirects;
+        console.log('Current URL:', this.currentUrl);
+      }
+    });
+  }
 }
