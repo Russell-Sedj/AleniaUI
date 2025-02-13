7f6d31d1087232c4e4e638928869f00a18b93ff8
Allan Khebab <allan.khebab@edu.igensia.com>
2025-02-13 11:34:49 +0100

    Amélioration de l'affichage des missions avec les noms des établissements et chargement dynamique des données

--- src/app/pages/missions/missions.component.ts
+++ src/app/pages/missions/missions.component.ts
@@ -1,9 +1,13 @@
+
+
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Mission } from '../../models/mission.model';
 import { MissionService } from '../../services/mission/mission.service';
+import { Etablissement } from '../../models/etablissement.model';
+import { EtablissementService } from '../../services/etablissement/etablissement.service';
 import { CommonModule } from '@angular/common';
 // import { AsyncPipe } from '@angular/common';
 
@@ -15,13 +19,26 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['../../../../public/assets/Css/style3.css'],
 })
 export class MissionsComponent implements OnInit {
-  missionList: Mission[] = [];
+  missionList: (Mission & { etablissement?: Etablissement })[] = [];
 
-  constructor(private missionService: MissionService) {}
+  constructor(
+    private missionService: MissionService,
+    private etablissementService: EtablissementService
+  ) {}
 
   ngOnInit(): void {
     this.missionService.getMissions().subscribe((response: any) => {
-      this.missionList = response.$values;
+      const missions = response.$values;
+      missions.forEach((mission: Mission) => {
+        this.etablissementService
+          .getEtablissement(mission.etablissementId)
+          .subscribe((etablissement: Etablissement) => {
+            this.missionList.push({
+              ...mission,
+              etablissement: etablissement
+            });
+          });
+      });
     });
   }
 }
