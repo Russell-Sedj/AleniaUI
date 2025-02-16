ee40dfda8626b281e4394686e35ec5d61af6e7e6
Russell-Sedj <34935151+Russell-Sedj@users.noreply.github.com>
2025-02-16 23:26:58 +0100

    Add additional fields to RegisterModel for enhanced user information

--- src/app/models/auth/register.model.ts
+++ src/app/models/auth/register.model.ts
@@ -2,4 +2,12 @@ export interface RegisterModel {
   email: string;
   motDePass: string;
   role: string;
+  pseudo?: string;
+  nom?: string;
+  prenom?: string;
+  adresse?: string;
+  telephone?: string;
+  typeEtablissement?: string;
+  competences?: string[];
+  disponibilites?: string;
 }
