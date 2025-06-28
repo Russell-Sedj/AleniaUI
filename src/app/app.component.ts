e9bd63f5728db8bae29e304377e2f84fe5a7fcf3
Allan Khebab <allan.khebab@edu.igensia.com>
2025-06-28 21:41:25 +0200

    fix : navbar et footer enlevé dans les dashboards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,6 @@ export class AppComponent implements OnInit {
   currentUrl: string = ''; // Initialisation de currentUrl avec une chaîne vide
 
   constructor(private router: Router) {}
-
   ngOnInit() {
     // Écouter les événements de navigation pour mettre à jour currentUrl
     this.router.events.subscribe((event) => {
@@ -24,5 +23,11 @@ export class AppComponent implements OnInit {
         this.currentUrl = event.urlAfterRedirects;
       }
     });
+  }
+
+  // Méthode pour détecter si on est sur une page de dashboard
+  isDashboardRoute(): boolean {
+    return this.currentUrl.includes('/dashboard-interimaire') || 
+           this.currentUrl.includes('/dashboard-etablissement');
   }
 }
